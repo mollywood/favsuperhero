8c7d18d128043ea0b129c045fafd6aec6f5b64a2
Molly Wood <mollywood@Interzone.localdomain>
2018-05-30 14:50:35 -0500

    first level shows up in html

--- site.js
+++ site.js
@@ -2,20 +2,24 @@ let filmsInfoDiv = document.querySelector('#filmsInfoDiv')
 
 let posts = []
 
-function populateFilms(filmsToDisplay) {
+function populateFilms(posts) {
+  console.log(posts, 'destination')
+  let filmsToDisplay = posts.Search
   filmsInfoDiv.innerHTML = ''
   filmsToDisplay.forEach(function(posts) {
     let filmsInfo =
-    '<a>${posts.title}</a>'
+    `<a>${posts.Title}</a>
+    <img src=${posts.Poster}>`
     filmsInfoDiv.innerHTML += filmsInfo
   })
 }
 
 addEventListener('load', function(){
-  fetch('http://www.omdbapi.com/?s=spiderman&apikey=cc1eb02f')
+  fetch('http://www.omdbapi.com/?s=batman&apikey=cc1eb02f')
   .then(function(response) {
     return response.json()
   }).then(function(posts){
-    console.log(posts)
-  }).then(populateFilms(posts))
+    console.log(posts.Search)
+    populateFilms(posts)
+  })
 })
